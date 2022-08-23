5f99d8811b04da68fda1c0b76b47ae4aaeee8ffa
benjamin kisenge <benkisenge03@gmail.com>
2022-08-23 22:09:31 +0200

    Get scores fromthe API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import './style.css';
+// import './style.css';
 import displayScore from './modules/displayScore.js';
 
 displayScore();
+
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zjli5jAyX6z7cxaxMGtk/scores/', {
+//   method: 'POST',
+//   body: JSON.stringify({
+// 	"user": "Ben kis",
+// 	"score": 100
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json)); 
--- src/modules/displayScore.js
+++ src/modules/displayScore.js
@@ -1,12 +1,12 @@
-import scores from './scores.js';
+import getScore from './scores.js';
 import htmlTemplate from './htmlTemplate.js';
 
 const scoreContainer = document.querySelector('.score-container');
-
-const displayScore = () => {
-  scores.forEach((score) => {
-    scoreContainer.insertAdjacentHTML('afterbegin', htmlTemplate(score));
-  });
+const scores = await getScore();
+const displayScore = () => { 
+    scores.forEach((score) => {
+        scoreContainer.insertAdjacentHTML('afterbegin', htmlTemplate(score)); 
+    });
 };
 
 export default displayScore;
