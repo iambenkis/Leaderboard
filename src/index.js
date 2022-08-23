a61c2e5cc81867f14313fc401a1f26df22b9abb5
benjamin kisenge <benkisenge03@gmail.com>
2022-08-23 22:56:43 +0200

    Add postToApi function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 // import './style.css';
 import displayScore from './modules/displayScore.js';
-
+const form = document.querySelector('form');
 displayScore();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputUser = form.querySelector('input[type = "text"]').value;
+    const inputScore = form.querySelector('input[type = "number"]').value;
+    console.log(inputUser)
+    console.log(inputScore)
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zjli5jAyX6z7cxaxMGtk/scores/', {
+        method: 'POST',
+        body: JSON.stringify({
+            "user": inputUser,
+            "score": inputScore
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    form.reset(); 
+})
 
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zjli5jAyX6z7cxaxMGtk/scores/', {
-//   method: 'POST',
-//   body: JSON.stringify({
-// 	"user": "Ben kis",
-// 	"score": 100
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
 //   .then((response) => response.json())
 //   .then((json) => console.log(json)); 
+
---
+++ src/modules/postToApi.js
@@ -0,0 +1,14 @@
+const postData = (user,score) => {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zjli5jAyX6z7cxaxMGtk/scores/', {
+        method: 'POST',
+        body: JSON.stringify({
+            "user": user,
+            "score": score
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+}
+
+export default postData;
