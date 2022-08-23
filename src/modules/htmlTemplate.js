8cedf0c8537f15da685253a848d9e862754c653a
benjamin kisenge <benkisenge03@gmail.com>
2022-08-24 00:42:35 +0200

    Update UI

--- src/modules/htmlTemplate.js
+++ src/modules/htmlTemplate.js
@@ -1,6 +1,9 @@
 const htmlTemplate = (item) => `
     <p>
-        <span class="name">${item.user}</span>
+        <span class="name"> 
+            <img src="/imgs/icon.png" alt="icon"> 
+            ${item.user}
+        </span>
         <span class="scores">${item.score}</span>
     </p> 
 `;
