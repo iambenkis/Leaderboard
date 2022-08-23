fe9eb8c037ec4a7f1838425702be6dfd94d8776f
benjamin kisenge <benkisenge03@gmail.com>
2022-08-23 16:12:20 +0200

    Add score

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -12,7 +12,23 @@ const scores = [
         score: 62,
     },
     {
-        name: 'Benjamin',
+        name: 'Mark',
+        score: 35,
+    },
+    {
+        name: 'John',
+        score: 35,
+    },
+    {
+        name: 'Joel',
+        score: 35,
+    },
+    {
+        name: 'James',
+        score: 35,
+    },
+    {
+        name: 'Ramos',
         score: 35,
     }
 ];
