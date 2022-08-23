b594150b9439737a4da6d319333b8d9729458dd6
benjamin kisenge <benkisenge03@gmail.com>
2022-08-23 14:06:52 +0200

    Create score array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // import './style.css';
+const scores = [
+    {
+        name: 'Benjamin',
+        score: 35,
+    },
+    {
+        name: 'George',
+        score: 56,
+    },
+    {
+        name: 'Dan',
+        score: 62,
+    },
+    {
+        name: 'Benjamin',
+        score: 35,
+    }
+]
